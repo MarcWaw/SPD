7bdade07c79cd6ca4d37dc0a4a6a7cd5f30a2e72
Marcin Wawszczak <235274@student.pwr.edu.pl>
2020-03-27 21:01:46 +0100

    Wyliczanie C_max - niegotowe

--- NEH/NEH/main.cpp
+++ NEH/NEH/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
 #include <vector>
 #include <cmath>
 #include <limits>
@@ -10,13 +9,35 @@
 #include <cstdlib>
 #include <chrono>
 
-#define MAX_TEST_NUMBER 121
+#define MAX_TEST_NUMBER 1
 
 using namespace std;
 
+struct taskp{
+	int id;
+	int priority;
+};
+
+//WYLICZANIE CMAX###############################################################################
+int calculate_c_max(int** matrix, int* permutation, int m, int n) {
+	int c_max = 0;
+
+	int *m_time = new int[m];
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (m_time[j] < matrix[permutation[i]][j])
+				m_time[j] = m_time[j] + matrix[permutation[i]][j];
+		}
+	}
+
+	delete[] m_time;
+	return c_max;
+}
+//##############################################################################################
+
 int main() {
 	ifstream data_file;
-	stringstream ss;
 	const char* file_name = "Data.txt";
 	string data_name[MAX_TEST_NUMBER];
 	
@@ -49,11 +70,11 @@ int main() {
 		cout << "Pomyslnie otwarto plik o nazwie: " << file_name << endl;
 		cout << "-------------------------------------------------------------------------" << endl;
 		auto experiment_start = chrono::steady_clock::now();
-		for (int i = 0; i < 8; i++) {
+		for (int i = 0; i < 1; i++) {
 			int m = 0;          // maszyny
 			int n = 0;          // zadania
 
-			int* priority_array;// tablica priorytetow zadan
+			taskp* priority_array;// tablica priorytetow zadan
 			int** time_matrix;  // macierz czasow
 
 			//WCZYTANIE ZESTAWU DANYCH######################################################################
@@ -75,39 +96,50 @@ int main() {
 				for (int b = 0; b < m; b++)
 					data_file >> time_matrix[a][b]; // a - wiersz b - kolumna
 			
-			/*
+			
 			//Wyswietl macierz
-			for (int a = 0; a < tasks; a++) {
-				for (int b = 0; b < machines; b++)
+			cout << endl <<  "Macierz czasow wykonywania: " << endl;
+			for (int a = 0; a < n; a++) {
+				for (int b = 0; b < m; b++)
 					cout << time_matrix[a][b] << " ";
 				cout << endl;
 			}
-			*/
+			
 			//##############################################################################################
 
 			//WYZNACZANIE PRIORYTETOW ZADAN#################################################################
-			priority_array = new int[n];
+			priority_array = new taskp[n];
 
 			for (int a = 0; a < n; a++) {
 				int priority_sum = 0;
 				for (int b = 0; b < m; b++) {
 					priority_sum = priority_sum + time_matrix[a][b];
 				}
-				priority_array[a] = priority_sum;
+				priority_array[a].id = a + 1;;
+				priority_array[a].priority = priority_sum;
 			}
 
-			/*
+			
 			//Wyswietl priorytet zadan
 			cout << endl << "Priorytety zadan: " << endl;
 			for (int a = 0; a < n; a++) {
-				cout << "Zadanie " << a + 1 << ": " << priority_array[a] << endl;
+				cout << "Zadanie " << priority_array[a].id << ": " << priority_array[a].priority << endl;
 			}
-			*/
+			
 
 			//##############################################################################################
 
 			//ALGORYTM NEH##################################################################################
 			auto search_start = chrono::steady_clock::now();
+			
+			for (int j = 0; j < n; j++) {
+				int* permutation = new int[j + 1];
+				for (int k = 0; k < j + 1; k++) {
+					permutation[k] = k;
+				}
+				cout << calculate_c_max(time_matrix, permutation, m, j + 1) << endl;
+			}
+
 
 			auto search_end = chrono::steady_clock::now();
 			//##############################################################################################
@@ -120,6 +152,7 @@ int main() {
 			for (int i = 0; i < n; ++i)
 				delete[] time_matrix[i];
 			delete[] time_matrix;
+			delete[] priority_array;
 		}
 		auto experiment_end = chrono::steady_clock::now();
 		cout << endl << "Obliczenia calosci zadanie trwaly: ";
